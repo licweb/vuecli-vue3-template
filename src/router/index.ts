d3bfc297863d1c44086ac825214742b183e46012
lichuan <licweb@qq.com>
2022-09-11 18:20:12 +0800

    项目初始化

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "@/views/HomeView.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
